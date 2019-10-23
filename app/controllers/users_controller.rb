7d33bd52f25b71eeb5e8733996307d930569d7e6
finneganz <babilon.extal1y@gmail.com>
2019-10-23 09:40:18 +0000

    Refactoring

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update]
   
   def show
     @user = User.find(params[:id])
@@ -27,7 +28,8 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     if @user.update_attributes(user_params)
-      #更新成功時の処理
+      flash[:success] = "Profile Updated"
+      redirect_to @user
     else
       render 'edit'
     end
@@ -39,5 +41,13 @@ class UsersController < ApplicationController
       params.require(:user).permit(:name, :email, :password, 
                                 :password_confirmation)
     end
-
+    
+    #beforeアクション
+    
+    def logged_in_user
+      unless logged_in?
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
+    end
 end
